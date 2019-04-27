e5bb1fca2d0c93931d205164b9694173d982c0ae
Mohamed Awad <awadmohamed233@gmail.com>
2019-04-27 13:43:39 +0200

    mail

--- contact.php
+++ contact.php
@@ -26,8 +26,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['name']) && !empty($_P
 		$mail->Port       = 587;                                    // TCP port to connect to
 	
 		//Recipients
-		//$mail->setFrom($_POST['email'], $_POST['email']);
-		$mail->From = $_POST['email'];
+		$mail->setFrom($_POST['email'], $_POST['email']);
+	
 		//$mail->addAddress('', 'Joe User');     // Add a recipient
 		$mail->addAddress('awadmohamed233@gmail.com');               // Name is optional
 		//$mail->addReplyTo('info@example.com', 'Information');
